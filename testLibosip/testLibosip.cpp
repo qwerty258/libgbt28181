3f367080635dd3b6cf4d172bfdcb60f3a5a53a44
qwerty258 <zyf26256@gmail.com>
2015-07-27 18:46:22 +0800

    add test code

--- testLibosip/testLibosip.cpp
+++ testLibosip/testLibosip.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
-
+#include <WinSock2.h>
+#include <osip2\osip.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+    osip_t* p_osip_t;
+
+    if(0 != osip_init(&p_osip_t))
+    {
+        return -1;
+    }
+
+    osip_free(p_osip_t);
+
+    return 0;
 }
 
