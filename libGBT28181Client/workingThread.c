7c04a10e0797d00d4a61098a313cec78e1990de4
qwerty258 <zyf26256@gmail.com>
2015-11-05 12:00:19 +0800

    1.Add keepalive thread. 2.Rename working threads.

--- libGBT28181Client/workingThread.c
+++ libGBT28181Client/workingThread.c
@@ -2,14 +2,14 @@
 #include "clientConfigDefine.h"
 #include <Windows.h>
 
-void* register_thread(void* arg)
+void* register_working_thread(void* arg)
 {
     client_configurations* thread_parameter = (client_configurations*)arg;
     int result;
 
     while(thread_parameter->thread_loop)
     {
-        Sleep((thread_parameter->expiration_interval - 10) * 1000);
+        osip_usleep((thread_parameter->expiration_interval - 10) * 1000 * 1000);
         eXosip_lock(thread_parameter->exosip_context);
         result = eXosip_register_send_register(
             thread_parameter->exosip_context,
@@ -27,7 +27,7 @@ void* register_thread(void* arg)
     return NULL;
 }
 
-void* event_thread(void* arg)
+void* event_working_thread(void* arg)
 {
     client_configurations* thread_parameter = (client_configurations*)arg;
     eXosip_event_t* event = NULL;
@@ -45,6 +45,11 @@ void* event_thread(void* arg)
         {
             case EXOSIP_REGISTRATION_SUCCESS:
                 printf("registrered successfully\n");
+                thread_parameter->keepalive_thread = osip_thread_create(20000, keepalive_working_thread, thread_parameter);
+                if(NULL == thread_parameter->keepalive_thread)
+                {
+                    // to do: handle error
+                }
                 break;
             case EXOSIP_REGISTRATION_FAILURE:
                 printf("registrered failed\n");
@@ -56,5 +61,85 @@ void* event_thread(void* arg)
     }
 
     osip_thread_exit();
+    return NULL;
+}
+
+void* keepalive_working_thread(void* arg)
+{
+    client_configurations* thread_parameter = (client_configurations*)arg;
+    int result = 0;
+    osip_message_t* keepalive_message = NULL;
+    char* from = NULL;
+    char* to = NULL;
+    char* message_body = NULL;
+
+    from = osip_malloc(512);
+    to = osip_malloc(512);
+    message_body = osip_malloc(512);
+
+    if(NULL == from || NULL == to || NULL == message_body)
+    {
+        osip_free(from);
+        osip_free(to);
+        osip_free(message_body);
+        return NULL;
+    }
+
+    snprintf(from, 512, "sip:%s@%s", thread_parameter->client_user_name, thread_parameter->client_IP);
+
+    snprintf(to, 512, "sip:%s@%s", thread_parameter->server_ID, thread_parameter->server_IP);
+
+    result = eXosip_message_build_request(
+        thread_parameter->exosip_context,
+        &keepalive_message,
+        "MESSAGE",
+        to,
+        from,
+        NULL);
+    if(OSIP_SUCCESS != result)
+    {
+        // to do: handle error
+        return NULL;
+    }
+
+    result = osip_message_set_content_type(keepalive_message, "application/MANSCDP+xml");
+    if(OSIP_SUCCESS != result)
+    {
+        // to do: handle error
+        return NULL;
+    }
+
+    while(thread_parameter->thread_loop)
+    {
+        snprintf(message_body, 512, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<Notify>\r\n<CmdType>Keepalive</CmdType>\r\n<SN>%u</SN>\r\n<DeviceID>%s</DeviceID><Status>OK</Status></Notify>", thread_parameter->MANSCDP_SN, thread_parameter->client_user_name);
+
+        result = osip_message_set_body(keepalive_message, message_body, strnlen(message_body, 512));
+        if(OSIP_SUCCESS != result)
+        {
+            // to do: handle error
+            return NULL;
+        }
+
+        eXosip_lock(thread_parameter->exosip_context);
+        result = eXosip_message_send_request(thread_parameter->exosip_context, keepalive_message);
+        eXosip_unlock(thread_parameter->exosip_context);
+        if(OSIP_SUCCESS != result)
+        {
+            // to do: handle error
+        }
+
+        EnterCriticalSection(&thread_parameter->critical_section);
+        thread_parameter->MANSCDP_SN++;
+        LeaveCriticalSection(&thread_parameter->critical_section);
+
+        osip_usleep(thread_parameter->heartbeat_interval * 1000 * 1000);
+    }
+
+    osip_message_free(keepalive_message);
+
+    osip_free(from);
+    osip_free(to);
+    osip_free(message_body);
+
     return NULL;
 }
--- libGBT28181Client/workingThread.h
+++ libGBT28181Client/workingThread.h
@@ -1,7 +1,8 @@
 #ifndef _WORKING_THREAD_H_
 #define _WORKING_THREAD_H_
 
-void* register_thread(void* arg);
-void* event_thread(void* arg);
+void* register_working_thread(void* arg);
+void* event_working_thread(void* arg);
+void* keepalive_working_thread(void* arg);
 
 #endif // !_WORKING_THREAD_H_
