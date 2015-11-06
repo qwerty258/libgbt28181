184c26480211840aaca9e23fe0f5b24e0d3fb02e
qwerty258 <zyf26256@gmail.com>
2015-11-06 14:27:38 +0800

    Add functions to XMLUtility.

--- libGBT28181Client/XMLUtility.c
+++ libGBT28181Client/XMLUtility.c
@@ -20,3 +20,53 @@ xmlDocPtr find_element(xmlDocPtr xml_document_pointer, char* element_name)
     }
     return NULL;
 }
+
+MANSCDP_xml_type get_xml_type(char* xml_type_string)
+{
+    if(NULL==xml_type_string)
+    {
+        return MANSCDP_xml_Unknown;
+    }
+
+    if(0 == xmlStrncmp(xml_type_string, "Control", xmlStrlen("Control")))
+    {
+        return MANSCDP_xml_Control;
+    }
+    if(0 == xmlStrncmp(xml_type_string, "Query", xmlStrlen("Query")))
+    {
+        return MANSCDP_xml_Query;
+    }
+    if(0 == xmlStrncmp(xml_type_string, "Notify", xmlStrlen("Notify")))
+    {
+        return MANSCDP_xml_Notify;
+    }
+    if(0 == xmlStrncmp(xml_type_string, "Response", xmlStrlen("Response")))
+    {
+        return MANSCDP_xml_Response;
+    }
+
+    return MANSCDP_xml_Unknown;
+}
+
+MANSCDP_command_type get_MANSCDP_command_type(char* MANSCDP_command_string)
+{
+    if(NULL == MANSCDP_command_string)
+    {
+        return MANSCDP_command_type_unknown;
+    }
+
+    if(0 == xmlStrncmp(MANSCDP_command_string, "DeviceInfo", xmlStrlen("DeviceInfo")))
+    {
+        return MANSCDP_DeviceInfo;
+    }
+    if(0 == xmlStrncmp(MANSCDP_command_string, "DeviceStatus", xmlStrlen("DeviceStatus")))
+    {
+        return MANSCDP_DeviceStatus;
+    }
+    if(0 == xmlStrncmp(MANSCDP_command_string, "Catalog", xmlStrlen("Catalog")))
+    {
+        return MANSCDP_Catalog;
+    }
+
+    return MANSCDP_command_type_unknown;
+}
--- libGBT28181Client/XMLUtility.h
+++ libGBT28181Client/XMLUtility.h
@@ -4,7 +4,10 @@
 #include <stdbool.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
+#include "clientConfigDefine.h"
 
-xmlDocPtr find_element(xmlDocPtr xml_document_pointer);
+xmlDocPtr find_element(xmlDocPtr xml_document_pointer, char* element_name);
+MANSCDP_xml_type get_xml_type(char* xml_type_string);
+MANSCDP_command_type get_MANSCDP_command_type(char* MANSCDP_command_string);
 
 #endif // _XMLUTILITY_H_
