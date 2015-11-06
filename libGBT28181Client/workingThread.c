8c3c26b7184b3f13afb9434c4a38037cc9a6aa25
qwerty258 <zyf26256@gmail.com>
2015-11-06 14:33:54 +0800

    1. Add MANSCDP xml processing thread. 2. Use compact xml. 3. Print exit tip when every thread exit.

--- libGBT28181Client/workingThread.c
+++ libGBT28181Client/workingThread.c
@@ -24,6 +24,9 @@ void* register_working_thread(void* arg)
     }
 
     osip_thread_exit();
+#ifdef _DEBUG
+    printf("register_working_thread exited\n");
+#endif // _DEBUG
     return NULL;
 }
 
@@ -35,6 +38,7 @@ void* event_working_thread(void* arg)
     osip_body_t* message_body = NULL;
     int result = OSIP_SUCCESS;
     xmlDocPtr xml_document_pointer = NULL;
+    xmlDocPtr xml_current_node = NULL;
 
     while(thread_parameter->thread_loop)
     {
@@ -78,6 +82,43 @@ void* event_working_thread(void* arg)
                         LIBXML_TEST_VERSION
 
                         xml_document_pointer = xmlReadMemory(message_body->body, message_body->length, "no_name.xml", NULL, 0);
+                        xml_current_node = xml_document_pointer;
+
+                        if(NULL != xml_current_node->children)
+                        {
+                            xml_current_node = xml_current_node->children;
+                            MANSCDP_xml* p_MANSCDP_xml = osip_malloc(sizeof(MANSCDP_xml));
+                            xmlDocPtr xmlDocPtr_temp = NULL;
+                            p_MANSCDP_xml->xml_type = get_xml_type(xml_current_node->name);
+                            p_MANSCDP_xml->p_client_configurations = thread_parameter;
+                            if(MANSCDP_xml_Unknown != p_MANSCDP_xml->xml_type)
+                            {
+                                xmlDocPtr_temp = find_element(xml_current_node->children, "CmdType");
+                                if(NULL != xmlDocPtr_temp)
+                                {
+                                    p_MANSCDP_xml->command_type = get_MANSCDP_command_type(xmlDocPtr_temp->children->content);
+                                }
+                                xmlDocPtr_temp = find_element(xml_current_node->children, "SN");
+                                if(NULL != xmlDocPtr_temp)
+                                {
+                                    p_MANSCDP_xml->MANSCDP_SN = strtoull(xmlDocPtr_temp->children->content, NULL, 10);
+                                }
+                                xmlDocPtr_temp = find_element(xml_current_node->children, "DeviceID");
+                                if(NULL != xmlDocPtr_temp)
+                                {
+                                    p_MANSCDP_xml->DeviceID = osip_strdup(xmlDocPtr_temp->children->content);
+                                }
+
+                                if(NULL == osip_thread_create(20000, MANSCDP_xml_message_working_thread, p_MANSCDP_xml))
+                                {
+                                    osip_free(p_MANSCDP_xml);
+                                }
+                            }
+                            else
+                            {
+                                osip_free(p_MANSCDP_xml);
+                            }
+                        }
 
                         xmlFreeDoc(xml_document_pointer);
 
@@ -86,8 +127,14 @@ void* event_working_thread(void* arg)
                 }
                 else
                 {
-
+                    // to do: response content not acceptable
                 }
+                break;
+            case EXOSIP_MESSAGE_ANSWERED:
+#ifdef _DEBUG
+                printf("SIP 200 OK, received\n");
+#endif // _DEBUG
+
                 break;
             default:
 #ifdef _DEBUG
@@ -99,6 +146,9 @@ void* event_working_thread(void* arg)
     }
 
     osip_thread_exit();
+#ifdef _DEBUG
+    printf("event_working_thread exited\n");
+#endif // _DEBUG
     return NULL;
 }
 
@@ -149,7 +199,7 @@ void* keepalive_working_thread(void* arg)
             return NULL;
         }
 
-        snprintf(message_body, 512, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<Notify>\r\n<CmdType>Keepalive</CmdType>\r\n<SN>%u</SN>\r\n<DeviceID>%s</DeviceID><Status>OK</Status></Notify>", thread_parameter->MANSCDP_SN, thread_parameter->client_user_name);
+        snprintf(message_body, 512, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Notify><CmdType>Keepalive</CmdType><SN>%u</SN><DeviceID>%s</DeviceID><Status>OK</Status></Notify>", thread_parameter->MANSCDP_SN, thread_parameter->client_user_name);
 
         result = osip_message_set_body(keepalive_message, message_body, strnlen(message_body, 512));
         if(OSIP_SUCCESS != result)
@@ -177,5 +227,111 @@ void* keepalive_working_thread(void* arg)
     osip_free(to);
     osip_free(message_body);
 
+#ifdef _DEBUG
+    printf("keepalive_working_thread exited\n");
+#endif // _DEBUG
+    return NULL;
+}
+
+void* MANSCDP_xml_message_working_thread(void* arg)
+{
+    MANSCDP_xml* p_MANSCDP_xml = (MANSCDP_xml*)arg;
+    int result = OSIP_SUCCESS;
+    osip_message_t* MANSCDP_xml_message = NULL;
+    char* from = osip_malloc(512);
+    char* to = osip_malloc(512);
+    char* xml_buffer = osip_malloc(1500);
+    if(NULL == from || NULL == to || NULL == xml_buffer)
+    {
+        osip_free(from);
+        osip_free(to);
+        osip_free(xml_buffer);
+        return NULL;
+    }
+    memset(xml_buffer, 0x0, 1500);
+
+    snprintf(from, 512, "sip:%s@%s", p_MANSCDP_xml->p_client_configurations->client_user_name, p_MANSCDP_xml->p_client_configurations->client_IP);
+    snprintf(to, 512, "sip:%s@%s", p_MANSCDP_xml->p_client_configurations->server_ID, p_MANSCDP_xml->p_client_configurations->server_IP);
+
+    switch(p_MANSCDP_xml->xml_type)
+    {
+        case MANSCDP_xml_Unknown:
+            break;
+        case MANSCDP_xml_Control:
+            break;
+        case MANSCDP_xml_Query:
+            switch(p_MANSCDP_xml->command_type)
+            {
+                case MANSCDP_command_type_unknown:
+                    break;
+                case MANSCDP_DeviceInfo:
+                    snprintf(xml_buffer, 1500, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><CmdType>DeviceInfo</CmdType><SN>%u</SN><DeviceID>%s</DeviceID><Result>OK</Result><DeviceType>libGBT28181Client</DeviceType><Manufacturer>libGBT28181Client</Manufacturer><Model>libGBT28181Client</Model><Firmware>libGBT28181Client</Firmware><MaxCamera>0</MaxCamera><MaxAlarm>0</MaxAlarm></Response>", p_MANSCDP_xml->MANSCDP_SN, p_MANSCDP_xml->p_client_configurations->client_user_name);
+                    break;
+                case MANSCDP_DeviceStatus:
+                    snprintf(xml_buffer, 1500, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><CmdType>DeviceStatus</CmdType><SN>%u</SN><DeviceID>%s</DeviceID><Result>OK</Result><Online>ONLINE</Online><Status>OK</Status><Alarmstatus Num=\"0\"></Alarmstatus></Response>", p_MANSCDP_xml->MANSCDP_SN, p_MANSCDP_xml->p_client_configurations->client_user_name);
+                    break;
+                case MANSCDP_Catalog:
+                    snprintf(xml_buffer, 1500, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><CmdType>Catalog</CmdType><SN>%u</SN><DeviceID>%s</DeviceID><SumNum>1</SumNum><DeviceList Num=\"1\"><Item><DeviceID>%s</DeviceID><Name>libGBT28181Client</Name><Manufacturer>libGBT28181Client</Manufacturer><Model>GBT28181Client</Model><Owner>Owner</Owner><CivilCode>CivilCode</CivilCode><Address>Address</Address><Parental>0</Parental><SafetyWay>0</SafetyWay><RegisterWay>1</RegisterWay><Secrecy>0</Secrecy><Status>ON</Status></Item></DeviceList></Response>", p_MANSCDP_xml->MANSCDP_SN, p_MANSCDP_xml->p_client_configurations->client_user_name, p_MANSCDP_xml->p_client_configurations->client_user_name);
+                    break;
+            }
+            break;
+        case MANSCDP_xml_Notify:
+            break;
+        case MANSCDP_xml_Response:
+            break;
+    }
+
+    while(true)
+    {
+        if(10 < xmlStrlen(xml_buffer))
+        {
+            result = eXosip_message_build_request(
+                p_MANSCDP_xml->p_client_configurations->exosip_context,
+                &MANSCDP_xml_message,
+                "MESSAGE",
+                to,
+                from,
+                NULL);
+            if(OSIP_SUCCESS != result)
+            {
+                // to do: handle error
+                break;
+            }
+
+            result = osip_message_set_content_type(MANSCDP_xml_message, "application/MANSCDP+xml");
+            if(OSIP_SUCCESS != result)
+            {
+                // to do: handle error
+                break;
+            }
+
+            result = osip_message_set_body(MANSCDP_xml_message, xml_buffer, strnlen(xml_buffer, 1500));
+            if(OSIP_SUCCESS != result)
+            {
+                // to do: handle error
+                break;
+            }
+
+            eXosip_lock(p_MANSCDP_xml->p_client_configurations->exosip_context);
+            result = eXosip_message_send_request(p_MANSCDP_xml->p_client_configurations->exosip_context, MANSCDP_xml_message);
+            eXosip_unlock(p_MANSCDP_xml->p_client_configurations->exosip_context);
+            if(OSIP_SUCCESS != result)
+            {
+                // to do: handle error
+                break;
+            }
+
+            break;
+        }
+    }
+
+    osip_free(from);
+    osip_free(to);
+    osip_free(xml_buffer);
+    osip_free(arg);
+
+#ifdef _DEBUG
+    printf("MANSCDP_xml_message_working_thread exited\n");
+#endif // _DEBUG
     return NULL;
 }
--- libGBT28181Client/workingThread.h
+++ libGBT28181Client/workingThread.h
@@ -4,5 +4,6 @@
 void* register_working_thread(void* arg);
 void* event_working_thread(void* arg);
 void* keepalive_working_thread(void* arg);
+void* MANSCDP_xml_message_working_thread(void* arg);
 
 #endif // !_WORKING_THREAD_H_
