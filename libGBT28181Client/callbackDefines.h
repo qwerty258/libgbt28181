e5fb280fe7e927089c86598a263b556c8b1ffca4
qwerty258 <zyf26256@gmail.com>
2015-11-09 17:33:54 +0800

    add callback function definition and data structure.

--- libGBT28181Client/callbackDefines.h
+++ libGBT28181Client/callbackDefines.h
@@ -21,4 +21,22 @@ typedef enum _MANSCDP_on_off_line
 
 typedef void(*function_query_device_status_callback)(char* device_ID, MANSCDP_on_off_line online, MANSCDP_result_type status);
 
+typedef struct _MANSCDP_device
+{
+    char* DeviceID;
+    char* Name;
+    char* Manufacturer;
+    char* Model;
+    char* Owner;
+    char* CivilCode;
+    char* Address;
+    char* Parental;
+    char* SafetyWay;
+    char* RegisterWay;
+    char* Secrecy;
+    char* Status;
+}MANSCDP_device;
+
+typedef void(*function_query_catalog_callback)(char* device_ID, unsigned long long sum_num, MANSCDP_device* p_MANSCDP_device_array);
+
 #endif // !_CALLBACK_DEFINES_H_
