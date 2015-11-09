12ce0c9f5f27791b4ec71dc509c9c0b71ec73532
qwerty258 <zyf26256@gmail.com>
2015-11-09 16:30:12 +0800

    add callback function

--- libGBT28181Client/callbackDefines.h
+++ libGBT28181Client/callbackDefines.h
@@ -3,14 +3,22 @@
 
 #include <stdbool.h>
 
-typedef enum _MANSCDP_result_type
-{
-    MANSCDP_OK,
-    MANSCDP_ERROR
-}result_type;
-
 typedef void(*function_query_device_info_callback)(char* device_ID, char* device_type, char* manufacturer, char* model, char* firmware, unsigned long long max_camera, unsigned long long max_alarm);
 
-typedef void(*function_query_device_status_callback)(char* device_ID, bool online, result_type status);
+typedef enum _MANSCDP_result_type
+{
+    MANSCDP_UNKNOWN_RESULT_TYPE,
+    MANSCDP_OK,
+    MANSCDP_ERROR
+}MANSCDP_result_type;
+
+typedef enum _MANSCDP_on_off_line
+{
+    MANSCDP_UNKNOWN_ON_OFF_LINE,
+    MANSCDP_ONLINE,
+    MANSCDP_OFFLINE
+}MANSCDP_on_off_line;
+
+typedef void(*function_query_device_status_callback)(char* device_ID, MANSCDP_on_off_line online, MANSCDP_result_type status);
 
 #endif // !_CALLBACK_DEFINES_H_
