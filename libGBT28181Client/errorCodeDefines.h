e84d3a8e2ac8f0b206f838f6b7e9c36862724d79
qwerty258 <zyf26256@gmail.com>
2016-01-15 18:02:37 +0800

    move error code to a header file

---
+++ libGBT28181Client/errorCodeDefines.h
@@ -0,0 +1,34 @@
+#ifndef _ERROR_CODE_DEFINE_H_
+#define _ERROR_CODE_DEFINE_H_
+
+#define GBT28181_SUCCESS                            0
+#define GBT28181_UNDEFINED_ERROR                    -1
+#define GBT28181_BADPARAMETER                       -2
+#define GBT28181_WRONG_STATE                        -3
+#define GBT28181_NOMEM                              -4
+#define GBT28181_SYNTAXERROR                        -5
+#define GBT28181_NOTFOUND                           -6
+#define GBT28181_API_NOT_INITIALIZED                -7
+#define GBT28181_NO_NETWORK                         -10
+#define GBT28181_PORT_BUSY                          -11
+#define GBT28181_UNKNOWN_HOST                       -12
+#define GBT28181_DISK_FULL                          -30
+#define GBT28181_NO_RIGHTS                          -31
+#define GBT28181_FILE_NOT_EXIST                     -32
+#define GBT28181_TIMEOUT                            -50
+#define GBT28181_TOOMUCHCALL                        -51
+#define GBT28181_WRONG_FORMAT                       -52
+#define GBT28181_NOCOMMONCODEC                      -53
+#define GBT28181_CLIENT_UNINITIALED                 -10000
+#define GBT28181_INTERVAL_TOO_SMALL                 -10001
+#define GBT28181_THREAD_CREATE_FAILED               -10002
+#define GBT28181_ESSENTIAL_CLIENT_PARAMETER_ERROR   -10003
+#define GBT28181_CALLBACK_NOTSET                    -10004
+#define GBT28181_ALREADY_ONLINE                     -10005
+#define GBT28181_MUST_ONLINE                        -10006
+#define GBT28181_HANDLE_DEPLETED                    -10007
+#define GBT28181_INVALID_HANDLE                     -10008
+#define GBT28181_ALREADY_STREAMING                  -10009
+#define GBT28181_WRONG_PTZ_CMD                      -10010
+
+#endif // !_ERROR_CODE_DEFINE_H_
